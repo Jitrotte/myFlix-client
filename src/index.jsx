779b8dd10c2266876d51a5fd96dea3f8a8dff89c
Jitrotte <jitrotte@gmail.com>
2022-06-29 16:05:57 -0500

    added mainview component

--- src/index.jsx
+++ src/index.jsx
@@ -1,19 +1,15 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import { MainView } from './components/main-view/main-view';
+import React from "react";
+import ReactDOM from "react-dom";
+import { MainView } from "./components/main-view/main-view";
 
-import './index.scss';
+import "./index.scss";
 
 class MyFlixApplication extends React.Component {
   render() {
-    return (
-      <div className="my-flix">
-        <div>Good morning</div>
-      </div>
-    )
+    return <MainView />;
   }
 }
 
-const container = document.getElementsByClassName('app-container')[0];
+const container = document.getElementsByClassName("app-container")[0];
 
 ReactDOM.render(React.createElement(MyFlixApplication), container);
